2af87dbe62b342b5f33a78d16fb65e57b79241b8
Amanda Baker <ambake@Amandas-MBP.guest.corp.microsoft.com>
2019-11-05 17:32:39 -0800

    Call respondWith sooner

--- sw.js
+++ sw.js
@@ -10,15 +10,17 @@ this.addEventListener('install', async (event) => {
     this.skipWaiting();
 });
 
-this.addEventListener('fetch', async (event) => {
-    let response = await caches.match(event.request);
-    if (response !== undefined) {
-        event.respondWith(response);
-    } else {
-        response = await fetch(event.request);
-        let responseCopy = response.clone();
-        const cache = await caches.open(CACHE);
-        cache.put(event.request, responseCopy);
-        event.respondWith(response);
-    }
+this.addEventListener('fetch', (event) => {
+    event.respondWith(async () => {
+        let response = await caches.match(event.request);
+        if (response !== undefined) {
+            return response;
+        } else {
+            response = await fetch(event.request);
+            let responseCopy = response.clone();
+            const cache = await caches.open(CACHE);
+            cache.put(event.request, responseCopy);
+            return response;
+        }
+    });
 });
