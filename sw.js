a7c1c09803d962ba4ccf6d0f0e944691f4f833ed
Amanda Baker <ambake@Amandas-MBP.guest.corp.microsoft.com>
2019-11-05 17:28:26 -0800

    SW handles fetch

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
+const CACHE = 'myCache';
 
 this.addEventListener('install', async (event) => {
-    const cache = await caches.open('myCache');
+    const cache = await caches.open(CACHE);
     cache.addAll([
         'index.html',
         'app.js',
         'style.css',
     ]);
     this.skipWaiting();
+});
+
+this.addEventListener('fetch', async (event) => {
+    let response = await caches.match(event.request);
+    if (response !== undefined) {
+        event.respondWith(response);
+    } else {
+        response = await fetch(event.request);
+        let responseCopy = response.clone();
+        const cache = await caches.open(CACHE);
+        cache.put(event.request, responseCopy);
+        event.respondWith(response);
+    }
 });
