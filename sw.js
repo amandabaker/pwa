f71459243a04292ea1f6126afeb8900ab62245aa
Amanda Baker <ambake@microsoft.com>
2019-11-06 15:09:10 -0800

    Ensure respondWith resolves properly

--- sw.js
+++ sw.js
@@ -1,25 +1,26 @@
 const CACHE = 'myCache';
 
+const cachedFiles = [
+    'index.html',
+    'app.js',
+    'style.css',
+];
+
+
 this.addEventListener('install', async (event) => {
-    const cache = await caches.open(CACHE);
-    cache.addAll([
-        'index.html',
-        'app.js',
-        'style.css',
-    ]);
+    await event.waitUntil(async () => {
+        const cache = await caches.open(CACHE);
+        cache.addAll(cachedFiles)
+    });
     this.skipWaiting();
 });
 
 this.addEventListener('fetch', (event) => {
-    event.respondWith(async function() {
-        try {
-            const response = await caches.match(event.request);
-            if (response) {
-                return response;
-            }
-        } catch (e) {
-            console.log(e);
-            return fetch(event.request);
+    event.respondWith(async () => {
+        const response = await caches.match(event.request);
+        if (response) {
+            return response;
         }
+        return fetch(event.request);
     });
 });
