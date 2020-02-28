f76847d46d65cdc151fff1baf7b8a860917edce9
Amanda Baker <ambake@microsoft.com>
2020-02-27 19:58:35 -0800

    Add sample using unsafe-area-top-inset-inline-start/end

---
+++ customTitleBar-inset-inline/app.js
@@ -0,0 +1,48 @@
+// // mac left, windows right. So this needs to work with either layout.
+
+const inlineStart = "--unsafe-area-top-inset-inline-start";
+const inlineEnd = "--unsafe-area-top-inset-inline-end";
+
+const height = 40;
+const width = 276;
+
+let isWindows = true;
+
+const setProperty = (name, value) => {
+  return document.documentElement.style.setProperty(name, value);
+}
+
+const updateCSSVars = () => {
+  if (isWindows) {
+    setProperty(inlineStart, `${window.innerWidth - width}px`);
+    setProperty(inlineEnd, 0);
+  } else { // mac
+    setProperty(inlineStart, 0);
+    setProperty(inlineEnd, `${window.innerWidth - width}px`);
+  }
+}
+
+updateCSSVars();
+window.addEventListener('resize', updateCSSVars);
+
+const toggleWinMac = () => {
+  const button = document.getElementById("toggleWinMac");
+  isWindows = !isWindows;
+  updateCSSVars();
+  button.textContent = isWindows ? "Emulate Mac Overlay" : "Emulate Windows Overlay";
+}
+document.getElementById("toggleWinMac").addEventListener("click", toggleWinMac);
+
+const registerServiceWorker = async () => {
+  try {
+      await navigator.serviceWorker.register('/pwa/customTitleBar-inset-inline/sw.js', 
+                                             { scope: '/pwa/customTitleBar-inset-inline/'});
+      console.log('Service worker registered');
+  } catch (e) {
+      console.log(`Registration failed: ${e}`);
+  }
+}
+
+if (navigator.serviceWorker) {
+  registerServiceWorker();
+}
