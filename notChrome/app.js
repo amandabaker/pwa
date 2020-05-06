617012f8e830bfa78f4dacfdd136e6ad83a2ade4
Amanda Baker <ambake@microsoft.com>
2020-05-06 13:02:57 -0700

    Fix SW registration path

--- notChrome/app.js
+++ notChrome/app.js
@@ -1,6 +1,6 @@
 const registerServiceWorker = async () => {
   try {
-      await navigator.serviceWorker.register('/pwa/customTitleBar/sw.js', { scope: '/pwa/customTitleBar/'});
+      await navigator.serviceWorker.register('/pwa/notChrome/sw.js', { scope: '/pwa/notChrome/'});
       console.log('Service worker registered');
   } catch (e) {
       console.log(`Registration failed: ${e}`);
