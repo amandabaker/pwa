817dc53b0b079846f1c4f865f44e2ecffd1d14c2
Hassan Talat <hatalat@microsoft.com>
2021-04-08 18:44:54 -0700

    moved directory

--- display-override-wco-empty/app.js
+++ display-override-wco-empty/app.js
@@ -1,7 +1,7 @@
 
 const registerServiceWorker = async () => {
     try {
-      await navigator.serviceWorker.register('/pwa/wco-empty/sw.js', { scope: '/pwa/wco-empty/'});
+      await navigator.serviceWorker.register('/pwa/display-override-wco-empty/sw.js', { scope: '/pwa/display-override-wco-empty/'});
       console.log('Service worker registered');
     } catch (e) {
       console.log(`Registration failed: ${e}`);
