70fedd471052aed1376436a7732b92fd8c095c86
Amanda Baker <ambake@microsoft.com>
2021-05-25 12:21:15 -0700

    Show CSS env()s as undefined instead of 0px when undefined

--- windowControlsOverlay-newCSSVars/app.js
+++ windowControlsOverlay-newCSSVars/app.js
@@ -36,12 +36,11 @@ const emulateMac = () => {
   updateWCOInfo();
 }
 
-// Logging stuff in UI should all go below here. 
-const updateWCOInfo = () => {
+const logJSBounds = () => {
   const wcoElement = document.getElementById('WCO');
   const visibleElement = document.getElementById('WCOVisible');
   const rectElement = document.getElementById('WCORect');
-  
+
   if (!navigator.windowControlsOverlay) {
     wcoElement.textContent = "navigator.windowControlsOverlay = undefined";
     visibleElement.textContent = "";
@@ -50,7 +49,7 @@ const updateWCOInfo = () => {
   }
 
   wcoElement.textContent = "navigator.windowControlsOverlay";
-  visibleElement.textContent = 
+  visibleElement.textContent =
       `visible = ${navigator.windowControlsOverlay.visible}`;
   const rect = navigator.windowControlsOverlay.getBoundingClientRect();
   rectElement.textContent =
@@ -64,28 +63,49 @@ const updateWCOInfo = () => {
   bottom: ${rect.bottom},
   left: ${rect.left}
 }`;
+}
 
+const logCSSRect = () => {
   const cssElement = document.getElementById('WCOCss');
   const cssElementTest = document.getElementById('WCOCssTest');
 
-// This mapping should match the #WCOCssTest style in ./style.css
-  const x_paddingLeft = getComputedStyle(cssElementTest).getPropertyValue('padding-left');
-  const width_paddingRight = getComputedStyle(cssElementTest).getPropertyValue('padding-right');
-  const y_paddingTop = getComputedStyle(cssElementTest).getPropertyValue('padding-top');
-  const height_paddingBottom= getComputedStyle(cssElementTest).getPropertyValue('padding-bottom');
+  // This mapping should match the #WCOCssTest style in ./style.css
+  let x = getComputedStyle(cssElementTest).getPropertyValue('padding-left');
+  if (x == "123.45px")
+    x = "undefined";
+
+  let width = getComputedStyle(cssElementTest).getPropertyValue('padding-right');
+  if (width == "123.45px")
+    width = "undefined";
+
+  let y = getComputedStyle(cssElementTest).getPropertyValue('padding-top');
+  if (y == "123.45px")
+    y = "undefined";
+
+  let height= getComputedStyle(cssElementTest).getPropertyValue('padding-bottom');
+  if (height == "123.45px")
+    height = "undefined";
+
   cssElement.textContent =
-`titlebar-area-x: ${x_paddingLeft},
-titlebar-area-width: ${width_paddingRight},
-titlebar-area-y: ${y_paddingTop},
-titlebar-area-height: ${height_paddingBottom},
+`titlebar-area-x: ${x},
+titlebar-area-width: ${width},
+titlebar-area-y: ${y},
+titlebar-area-height: ${height},
 `;
 
-  const cssError = document.getElementById("CSSEnvsEvaluateTo0");
-  if (height_paddingBottom == "0px" && width_paddingRight == "0px") {
-    cssError.style.visibility = "visible";
+  // Show warning if the CSS env()s evaluate to an empty rect.
+  const cssEmpty = document.getElementById("CSSEnvsEmptyRect");
+  if (height == "0px" && width == "0px") {
+    cssEmpty.style.visibility = "visible";
   } else {
-    cssError.style.visibility = "hidden";
+    cssEmpty.style.visibility = "hidden";
   }
+}
+
+// Logging stuff in UI should all go below here.
+const updateWCOInfo = () => {
+  logJSBounds();
+  logCSSRect();
 
   const geometrychangeCountElement = document.getElementById('WCOGeometrychangeCount');
   geometrychangeCountElement.textContent = `geometrychange count: ${geometrychangeCount}`;
@@ -103,7 +123,7 @@ try {
                                                    onGeometryChange);
 } catch (e) {
   console.error(e);
-  
+
   // Show errors in webpage so it's obvious that something is broken.
   const errorDiv = document.getElementById("GeometryChangeListenerError");
   errorDiv.style.visibility = "visible";
