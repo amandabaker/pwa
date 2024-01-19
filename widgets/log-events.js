f6ddfff708c72f63efc17e2af3bf51c3a25ec3c2
Amanda Baker <ambake@microsoft.com>
2024-01-18 19:36:22 -0800

    Log a running count of widget events

--- widgets/log-events.js
+++ widgets/log-events.js
@@ -1,23 +1,39 @@
 
-const dbName = "event_logs";
-const objectStoreName = "events";
+const dbName = "eventLogs";
+const dbVersion = 1;
+
+/**
+ * Events are logged in "events" as:
+ *   key: auto-incremented,
+ *   value: {
+ *     type: [widget event type, e.g. "widgetresume"],
+ *     time: [time event was fired],
+ *   }
+ */
+const eventsOSName = "events";
+/**
+ * Event count is logged in "eventCount" as:
+ *   key: [widget event type, e.g. "widgetresume"],
+ *   value: {
+ *     count: [total count of that event type],
+ *     countAtLastAppLaunch:
+ *         [this is reset to |count| whenever an in-scope Window is opened],
+ *   }
+ */
+const eventCountOSName = "eventCount";
 
 const logWidgetEvent = async (eventName, time) => {
-  const widgetEventInfo = {
-    type: eventName,
-    time: time,
-  };
-
-  const request = indexedDB.open(dbName);
+  const request = indexedDB.open(dbName, dbVersion);
 
   request.onerror = (event) => {
     console.log(`Failed to open db with: ${event.errorCode}`)
   };
 
   request.onsuccess = (event) => {
-    let db = event.target.result;
-    const transaction =
-        db.transaction(objectStoreName, "readwrite", { durability: "strict"});
+    const db = event.target.result;
+    const transaction = db.transaction([ eventsOSName, eventCountOSName ],
+                                       "readwrite",
+                                       { durability: "strict"});
 
     transaction.oncomplete = (event) => {
       console.log("widget event logged to IndexedDB");
@@ -27,15 +43,51 @@ const logWidgetEvent = async (eventName, time) => {
       console.log(event.errorCode);
     }
 
-    const objectStore = transaction.objectStore(objectStoreName);
-    const request = objectStore.add(widgetEventInfo);
+    // Log the event and the time it was fired.
+    const widgetEventInfo = {
+      type: eventName,
+      time: time,
+    };
+    const eventsOS = transaction.objectStore(eventsOSName);
+    eventsOS.add(widgetEventInfo);
+
+    // Increment the total count of events of this type fired.
+    const eventCountOS = transaction.objectStore(eventCountOSName);
+    const getRequest = eventCountOS.get(eventName);
+    getRequest.onsuccess =  (event) => {
+      let oldEventCountInfo = getRequest.result;
+      if (!oldEventCountInfo) {
+        eventCountOS.add({
+          type: eventName,
+          count: 1,
+          countAtLastAppLaunch: 0,
+        });
+      } else {
+        const newEventCountInfo = {
+          ...oldEventCountInfo,
+          count: ++oldEventCountInfo.count,
+        };
+        eventCountOS.put(newEventCountInfo);
+      }
+    }
+    getRequest.onerror = (event) => {
+      console.log(`Error when getting ${eventName} from "${eventCountOSName}": ${getRequest.errorCode}`);
+    }
   };
 
   request.onupgradeneeded = (event) => {
     const db = event.target.result;
-    const objectStore = db.createObjectStore("events", { autoIncrement: true });
 
-    objectStore.createIndex("type", "type", { unique: false });
-    objectStore.createIndex("time", "time", { unique: false });
+    const eventsOS =
+        db.createObjectStore(eventsOSName, { autoIncrement: true });
+    eventsOS.createIndex("type", "type", { unique: false });
+    eventsOS.createIndex("time", "time", { unique: false });
+
+    const eventCountOS =
+        db.createObjectStore(eventCountOSName, { keyPath: "type" });
+    eventCountOS.createIndex("count", "count", { unique: false });
+    eventCountOS.createIndex("countAtLastAppLaunch",
+                             "countAtLastAppLaunch",
+                             { unique: false });
   }
 }
